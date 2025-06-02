a56e715df6e9e1643a9798bcc9496cbcbc722049
Takaaki Hirano <hivedark@gmail.com>
2025-06-02 15:48:04 +0900

    Mod Mason config.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,3 @@
-local bind = vim.keymap.set
-
 return {
   {
     'mason-org/mason.nvim',
@@ -9,18 +7,10 @@ return {
   },
   { 'mason-org/mason-lspconfig.nvim',
     config = function()
-      require('mason-lspconfig').setup()
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'phpactor' }
+      })
     end
   },
-  {
-    'neovim/nvim-lspconfig',
-    init = function()
-      bind('n', '<Leader>dd', 'vim.lsp.buf.definition')
-    end,
-    config = function()
-      vim.lsp.handlers['texDocument/publishDiagnostics'] = vim.lsp.with(
-        vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
-      )
-    end
-  },
+  { 'neovim/nvim-lspconfig' },
 }
